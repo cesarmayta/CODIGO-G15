da034f5be97c1fe82969a3d0731fbb55152b4632
cesar mayta <cesarmayta@gmail.com>
2022-06-16 19:47:58 -0500

    dia4

---
+++ semana01/dia4/clases.py
@@ -0,0 +1,30 @@
+#crear una clase
+class Automovil:
+    #crear atributos
+    def __init__(self,aa,pl,col,mar):
+        self.año = aa
+        self.placa = pl
+        self.color = col
+        self.marca = mar
+
+    #crear metodos
+    def encender(self):
+        print('encender ' + self.marca)
+
+    def avanzar(self):
+        print('avanzar' + self.marca)
+
+    def acelerar(self):
+        print('acelerar ' + self.marca)
+
+    def frenar(self):
+        print('frenar ' + self.marca)
+
+#creación de objetos
+vw = Automovil(1970,'CH-1234','Amarillo','Volkswagen')
+print("se creo el objeto vw con los siguiente datos : ")
+print(" año : " + str(vw.año))
+print(" color : " + vw.color)
+print(" placa : " + vw.placa)
+print(" maraca : " + vw.marca)
+
