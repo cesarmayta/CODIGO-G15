570c1ddf925bf0a0c73f7895d40ff9d5a7aee0c9
cesar mayta <cesarmayta@gmail.com>
2022-06-16 22:41:40 -0500

    dia 4 paleta

--- semana01/dia4/videojuego/main.py
+++ semana01/dia4/videojuego/main.py
@@ -27,12 +27,35 @@ class Bolita(pygame.sprite.Sprite):
             self.speed[0] = -self.speed[0]
         self.rect.move_ip(self.speed)
 
+class Paleta(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('paleta.png')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (ANCHO / 2, ALTO - 20)
+        self.speed = [0,0]
+
+    def update(self,evento):
+        #Buscar si se presiono la fecha de izquierda
+        if evento.key == pygame.K_LEFT and self.rect.left > 0:
+            self.speed = [-5,0]
+        elif evento.key == pygame.K_RIGHT and self.rect.right < ANCHO:
+            self.speed = [5,0]
+        else:
+            self.speed = [0,0]
+
+        self.rect.move_ip(self.speed)
+
 pantalla = pygame.display.set_mode((ANCHO,ALTO))
 pygame.display.set_caption('mi primer videojuego en python')
 
 reloj = pygame.time.Clock()
 
+#ajustar repetición de evento de tecla presionada
+pygame.key.set_repeat(30)
+
 bolita = Bolita()
+jugador = Paleta()
 
 while True:
     #establecer el reloj
@@ -41,10 +64,13 @@ while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
+        elif evento.type == pygame.KEYDOWN:
+            jugador.update(evento)
     #mover  la bolita
     bolita.update()
     #pintar la pantalla
     pantalla.fill(FONDO)
     #dibujar la bolita en la pantalla
     pantalla.blit(bolita.image,bolita.rect)
+    pantalla.blit(jugador.image,jugador.rect)
     pygame.display.flip()
