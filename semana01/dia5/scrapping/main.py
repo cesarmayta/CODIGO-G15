97e1ddb1a2fdb39d2dba69567c2f3f734fabc6ff
cesar mayta <cesarmayta@gmail.com>
2022-06-17 22:50:31 -0500

    dia5 scrapping

--- semana01/dia5/scrapping/main.py
+++ semana01/dia5/scrapping/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 
 url = requests.get("https://www.sbs.gob.pe/app/pp/SISTIP_PORTAL/Paginas/Publicacion/TipoCambioPromedio.aspx")
 
@@ -11,18 +12,35 @@ def scrapping_tipocambio():
         html = BeautifulSoup(url.text,'html.parser')
         tabla = html.find_all('table',{'id':'ctl00_cphContent_rgTipoCambio_ctl00'})
         #print(tabla)
-        filaDolar = html.find_all('tr',{'id':'ctl00_cphContent_rgTipoCambio_ctl00__0'})
+        #filaDolar = html.find_all('tr',{'id':'ctl00_cphContent_rgTipoCambio_ctl00__0'})
         #print(filaDolar)
+        """
         moneda = filaDolar[0].find('td',{'class':'APLI_fila3'})
         compra = filaDolar[0].find('td',{'class':'APLI_fila2'})
         venta = filaDolar[0].find('td',{'class':'APLI_fila2'}).findNext('td')
         print(moneda.get_text())
         print(compra.get_text())
         print(venta.get_text())
-
         cambio_valores = filaDolar[0].find_all('td',{'class':'APLI_fila2'})
         print(cambio_valores[0].get_text())
         print(cambio_valores[1].get_text())
+        """
+        listaMonedas = []
+        for i in range(7):
+            fila = html.find('tr',{'id':'ctl00_cphContent_rgTipoCambio_ctl00__'+str(i)}) 
+            moneda = fila.find('td',{'class':'APLI_fila3'})
+            compra = fila.find('td',{'class':'APLI_fila2'})
+            venta = fila.find('td',{'class':'APLI_fila2'}).findNext('td')
+            dicMoneda = {
+                'moneda': moneda.get_text(),
+                'compra': compra.get_text(),
+                'venta': venta.get_text()
+            }
+            listaMonedas.append(dicMoneda)
+        
+        columnas = ["moneda","compra","venta"]
+        tablaMonedas = [moneda.values() for moneda in listaMonedas]
+        print(tabulate(tablaMonedas,headers=columnas,tablefmt="grid"))
     else:
         print("error " + str(url.status_code))
 
