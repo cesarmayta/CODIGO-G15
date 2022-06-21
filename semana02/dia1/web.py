21c4369fdefc638f35f50da40470453e88156bff
cesar mayta <cesarmayta@gmail.com>
2022-06-20 21:35:17 -0500

    dia1 request

--- semana02/dia1/web.py
+++ semana02/dia1/web.py
@@ -12,5 +12,17 @@ def saludo():
     nombre = request.args.get('nombre','no hay nombre')
     return '<center><b>Hola {}</b></center>'.format(nombre)
 
+@app.route('/suma')
+def suma():
+    n1 = request.args.get('n1','0')
+    n2 = request.args.get('n2','0')
+    resultado = int(n1) + int(n2)
+    return '<center><b>la suma de {} + {} es {}</b></center>'.format(n1,n2,resultado)
+
+@app.route('/resta/<int:n1>/<int:n2>')
+def resta(n1=0,n2=0):
+    resultado = n1 - n2
+    return '<center><b>la resta de {} - {} es {}</b></center>'.format(n1,n2,resultado)
+
 app.run(debug=True)
 
