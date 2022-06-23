810033d365513afe6f08812c4ce2de1efab7a5c8
cesar mayta <cesarmayta@gmail.com>
2022-06-22 20:46:01 -0500

    dia3 firebase

---
+++ semana02/dia3/pruebafirebase.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("token.json")
+firebase_admin.initialize_app(cred)
+
+from firebase_admin import firestore
+
+db = firestore.client()
+
+colProyectos = db.collection('proyectos')
+docProyectos = colProyectos.get()
+
+for doc in docProyectos:
+    print(doc.to_dict())
