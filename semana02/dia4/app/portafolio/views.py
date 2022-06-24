0a05851c798b0b1f160cfa7eec019a711f4ecb81
cesar mayta <cesarmayta@gmail.com>
2022-06-23 20:43:41 -0500

    dia4 portafolio en app factory listo

--- semana02/dia4/app/portafolio/views.py
+++ semana02/dia4/app/portafolio/views.py
@@ -9,3 +9,21 @@ def index():
     dicBiografia = fb.getCollection('biografia')[0]
     session['biografia'] = dicBiografia
     return render_template('portafolio/index.html')
+
+@portafolio.route('/proyectos')
+def proyectos():
+    listaProyectos = fb.getCollection('proyectos')
+    
+    context = {
+        'proyectos':listaProyectos
+    }
+
+    return render_template('portafolio/portafolio.html',**context)
+
+@portafolio.route('/acercade')
+def acercade():
+    return render_template('portafolio/acercade.html')
+
+@portafolio.route('/contacto')
+def contacto():
+    return render_template('portafolio/contacto.html')
