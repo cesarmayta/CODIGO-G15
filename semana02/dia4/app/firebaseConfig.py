a60aa7ada12b695dddd09bf787d7188d7013eaaf
César Mayta <cesarmayta@gmail.com>
2022-06-23 21:57:02 -0500

    Delete firebaseConfig.py

--- semana02/dia4/app/firebaseConfig.py
+++
@@ -1,9 +0,0 @@
-firebaseConfig = {
-  "apiKey": "AIzaSyCenSdORL-44HSmd3Qu1VwvH48yUTDu7wQ",
-  "authDomain": "portafoliog15.firebaseapp.com",
-  "projectId": "portafoliog15",
-  "storageBucket": "portafoliog15.appspot.com",
-  "messagingSenderId": "847358946729",
-  "appId": "1:847358946729:web:02fe875796af126db128b8",
-  "measurementId": "G-KN2LNCK18Y"
-}
