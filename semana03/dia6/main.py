ca60b0e61fad70a397a728fa3437bf4ee1b3c60c
cesar mayta <cesarmayta@gmail.com>
2022-07-04 20:22:22 -0500

    dia 6 variables de entorno

--- semana03/dia6/main.py
+++ semana03/dia6/main.py
@@ -1,9 +1,35 @@
 from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from dotenv import load_dotenv
 import os
+from os.path import join,dirname
+
+dotenv_path = join(dirname(__file__),'.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#CREAMOS UNA CLASE QUE REPRENTE A UNA TABLA
+class Alumno(db.Model):
+    id = db.Column(db.Integer,primary_key=True) #ID INT PRIMARY_KEY
+    nombre = db.Column(db.String(100),nullable=False) #nombre varchar(100)
+    email = db.Column(db.String(100),unique=True) #email varchar(100)
+
+    def __init__(self,nombre,email):
+        self.nombre = nombre
+        self.email = email
+
+#MIGRAMOS LAS CLASES A LA BASE DE DATOS CONVIERTIENDOLAS EN TABLAS
+db.create_all()
+print("se crearon las tablas en la base de datos")
+
+
 
 @app.route('/')
 def index():
