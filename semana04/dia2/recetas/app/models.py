ea3bc92b55324f5f0bbca4d4df68e0789dffc062
cesar mayta <cesarmayta@gmail.com>
2022-07-05 20:45:46 -0500

    dia2 modelos

--- semana04/dia2/recetas/app/models.py
+++ semana04/dia2/recetas/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Receta(models.Model):
+    titulo =  models.CharField(max_length=100)
+    ingredientes = models.TextField(help_text='ingresa los ingredientes')
+    preparacion = models.TextField(help_text='ingresa los pasos de preparación')
+    tiempo_registro = models.DateTimeField(auto_now=True)
+    autor = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
