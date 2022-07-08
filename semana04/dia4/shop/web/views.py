d2e7cb08519d6075312c15b553d9e4c31177e887
cesar mayta <cesarmayta@gmail.com>
2022-07-07 21:39:21 -0500

    dia4 catalogo

--- semana04/dia4/shop/web/views.py
+++ semana04/dia4/shop/web/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from .models import Categoria,Marca,Producto
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    listaProductos = Producto.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    return render(request,'index.html',context)
 
 def producto(request):
     return render(request,'producto.html')
