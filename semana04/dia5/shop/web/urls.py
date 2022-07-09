fe5fa02eb3a19a30951a7e8fa479e530578326e7
cesar mayta <cesarmayta@gmail.com>
2022-07-08 20:12:49 -0500

    dia5 carrito

---
+++ semana04/dia5/shop/web/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'web'
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('producto/<int:producto_id>',views.producto,name='producto'),
+    path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria,name='productosPorCategoria'),
+    path('productosPorMarca/<int:marca_id>',views.productosPorMarca,name='productosPorMarca'),
+    path('productosPorNombre',views.productosPorNombre,name='productosPorNombre'),
+    path('carrito',views.carrito,name='carrito'),
+    path('agregarCarrito/<int:producto_id>',views.agregarCarrito,name='agregarCarrito'),
+    path('login',views.login,name='login')
+]
---
+++ semana04/dia5/shop/web/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+
+from .models import Categoria,Marca,Producto
+
+from .carrito import Cart
+
+################### CATALOGO DE PRODUCTOS
+def index(request):
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    listaProductos = Producto.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    return render(request,'index.html',context)
+
+def producto(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    return render(request,'producto.html',context)
+
+def productosPorCategoria(request,categoria_id):
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+def productosPorMarca(request,marca_id):
+    objMarca = Marca.objects.get(pk=marca_id)
+    listaProductos = objMarca.producto_set.all()
+
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+def productosPorNombre(request):
+    nombre = request.POST['nombre']
+    print('valor a buscar : ' + nombre)
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+
+############################ CARRITO DE COMPRAS ############
+
+def carrito(request):
+    return render(request,'carrito.html')
+
+def agregarCarrito(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,1)
+    print(request.session.get("cart"))
+
+    return render(request,'carrito.html')
+
+
+############################# CUENTA DE USUARIO
+def login(request):
+    return render(request,'login.html')
