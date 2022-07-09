a06f6df762dbfad58e9e6ca51895fa1e36fe4de2
cesar mayta <cesarmayta@gmail.com>
2022-07-09 10:20:57 -0500

    dia6 cuenta cliente

---
+++ semana04/dia6/shop/web/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=200)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria,on_delete=models.RESTRICT)
+    marca = models.ForeignKey(Marca,on_delete=models.RESTRICT)
+    sku = models.CharField(max_length=20,null=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True)
+    peso = models.IntegerField(default=0,null=True)
+    dimension = models.CharField(max_length=100,null=True)
+    color = models.CharField(max_length=200,null=True)
+    precio = models.DecimalField(max_digits=9,decimal_places=2)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='productos',blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+from django.contrib.auth.models import User
+
+class Cliente(models.Model):
+    usuario = models.OneToOneField(User,on_delete=models.CASCADE)
+    dni = models.CharField(max_length=8)
+    sexo = models.CharField(max_length=1,default='M')
+    telefono = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField(null=True)
+    direccion = models.TextField()
+
+    def __str__(self):
+        return self.dni
+
+
---
+++ semana04/dia6/shop/web/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import render,redirect
+
+from .models import Categoria,Marca,Producto
+
+from .carrito import Cart
+
+from .forms import ClienteForm
+
+################### CATALOGO DE PRODUCTOS
+def index(request):
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    listaProductos = Producto.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    return render(request,'index.html',context)
+
+def producto(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    return render(request,'producto.html',context)
+
+def productosPorCategoria(request,categoria_id):
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+def productosPorMarca(request,marca_id):
+    objMarca = Marca.objects.get(pk=marca_id)
+    listaProductos = objMarca.producto_set.all()
+
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+def productosPorNombre(request):
+    nombre = request.POST['nombre']
+    print('valor a buscar : ' + nombre)
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+
+    listaCategorias = Categoria.objects.all()
+    listaMarcas = Marca.objects.all()
+    context = {
+        'marcas':listaMarcas,
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+
+    return render(request,'index.html',context)
+
+
+############################ CARRITO DE COMPRAS ############
+
+def carrito(request):
+    return render(request,'carrito.html')
+
+def agregarCarrito(request,producto_id):
+    if request.method == 'POST':
+        cantidad = int(request.POST['cantidad'])
+    else:
+        cantidad  = 1
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto,cantidad)
+    print(request.session.get("cart"))
+
+    return render(request,'carrito.html')
+
+def eliminarProductoCarrito(request,producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request,'carrito.html')
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+
+    return render(request,'carrito.html')
+    
+############################# CUENTA DE USUARIO
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+
+def crearUsuario(request):
+    if request.method == "POST":
+        dataUsuario = request.POST['nuevoUsuario']
+        dataPassword = request.POST['nuevoPassword']
+
+        nuevoUsuario = User.objects.create_user(username=dataUsuario,password=dataPassword)
+        if nuevoUsuario is not None:
+            login(request,nuevoUsuario)
+            return redirect('/cuenta')
+        
+
+        return render(request,'login.html')
+
+def loginUsuario(request):
+    context = {}
+    if request.method == 'POST':
+        #login de usuarios
+        dataUsuario = request.POST['usuario']
+        dataPassword = request.POST['password']
+
+        usuarioAuth = authenticate(request,username=dataUsuario,password=dataPassword)
+        if usuarioAuth is not None:
+            login(request,usuarioAuth)
+            return redirect('/cuenta')
+        else:
+            context = {
+                'error':'datos incorrectos'
+            }
+
+    return render(request,'login.html',context)
+
+def logoutUsuario(request):
+    logout(request)
+    return render(request,'login.html')
+
+def cuentaUsuario(request):
+    frmCliente = ClienteForm()
+
+    context = {
+        'frmCliente' : frmCliente
+    }
+
+    return render(request,'cuenta.html',context)
