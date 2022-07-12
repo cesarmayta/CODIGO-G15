9a967feef31494cce6c1b2c9722fee8b1b4284bd
cesar mayta <cesarmayta@gmail.com>
2022-07-11 20:14:09 -0500

    dia1 djangorestframework

--- semana05/dia1/alumnos/api/views.py
+++ semana05/dia1/alumnos/api/views.py
@@ -1,9 +1,28 @@
+from multiprocessing.dummy import JoinableQueue
 from django.http import JsonResponse
 
 # Create your views here.
+from .models import Alumno
 
 def index(request):
     context = {
         'mensaje':'Bienvenido a mi API con django'
+    }
+    return JsonResponse(context)
+
+def alumnos(request):
+    dataAlumnos = Alumno.objects.all()
+    #serializamos los resultados
+    listAlumnos = []
+    for alumno in dataAlumnos:
+        dicAlumno = {
+            'nombre':alumno.nombre,
+            'email':alumno.email
+        }
+        listAlumnos.append(dicAlumno)
+
+    context = {
+        'status':True,
+        'content':listAlumnos
     }
     return JsonResponse(context)
