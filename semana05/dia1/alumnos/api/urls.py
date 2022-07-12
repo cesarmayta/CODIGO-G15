25d4cf5f3476c1b1159fd420176c196cc04584ce
cesar mayta <cesarmayta@gmail.com>
2022-07-11 20:47:31 -0500

    dia1 serializers

--- semana05/dia1/alumnos/api/urls.py
+++ semana05/dia1/alumnos/api/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('alumno',views.alumnos)
+    path('alumno',views.alumnos),
+    path('home',views.home),
+    path('getalumno',views.getAlumnos),
+    path('setalumno',views.setAlumno)
 ]
--- semana05/dia1/alumnos/api/views.py
+++ semana05/dia1/alumnos/api/views.py
@@ -1,5 +1,8 @@
-from multiprocessing.dummy import JoinableQueue
 from django.http import JsonResponse
+
+#para trabajar con djangorestframework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 from .models import Alumno
@@ -9,6 +12,15 @@ def index(request):
         'mensaje':'Bienvenido a mi API con django'
     }
     return JsonResponse(context)
+
+@api_view(['GET'])
+def home(request):
+    context = {
+        'status':True,
+        'content':'Bienvenido a mi API con DRF'
+    }
+
+    return Response(context)
 
 def alumnos(request):
     dataAlumnos = Alumno.objects.all()
@@ -26,3 +38,33 @@ def alumnos(request):
         'content':listAlumnos
     }
     return JsonResponse(context)
+
+######### aplicando serializers de drf #########
+
+from .serializers import AlumnoSerializer
+
+@api_view(['GET'])
+def getAlumnos(request):
+    dataAlumnos = Alumno.objects.all()
+    serAlumnos = AlumnoSerializer(dataAlumnos,many=True)
+
+    context = {
+        'status':True,
+        'content':serAlumnos.data
+    }
+
+    return Response(context)
+
+@api_view(['POST'])
+def setAlumno(request):
+    serAlumno = AlumnoSerializer(data=request.data)
+    serAlumno.is_valid(raise_exception=True)
+
+    nuevoAlumno = serAlumno.save()
+
+    context = {
+        'status':True,
+        'content':AlumnoSerializer(nuevoAlumno).data
+    }
+
+    return Response(context)
