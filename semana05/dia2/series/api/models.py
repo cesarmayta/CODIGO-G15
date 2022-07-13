d74844cf4767bd40229cdfdb67c6e9beb26338ae
cesar mayta <cesarmayta@gmail.com>
2022-07-12 20:00:38 -0500

    dia2 get series

--- semana05/dia2/series/api/models.py
+++ semana05/dia2/series/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Serie(models.Model):
+
+    CATEGORIAS_CHOICES = (
+        ('horror','Horror'),
+        ('comedia','Comedia'),
+        ('accion','Acción'),
+        ('drama','Drama')
+    )
+
+    nombre = models.CharField(max_length=200)
+    fecha_registro = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    categoria = models.CharField(max_length=10,choices=CATEGORIAS_CHOICES)
+
+    def __str__(self):
+        return self.nombre
+
--- semana05/dia2/series/api/views.py
+++ semana05/dia2/series/api/views.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
+
+from .models import Serie
+from .serializers import SerieSerializer
+
 class IndexView(APIView):
 
     def get(self,request):
@@ -9,3 +14,12 @@ class IndexView(APIView):
             'content':'servidor activo'
         }
         return Response(context)
+
+    
+
+class SeriesView(APIView):
+
+    def get(self,request):
+        dataSeries = Serie.objects.all()
+        serSeries = SerieSerializer(dataSeries,many=True)
+        return Response(serSeries.data)
