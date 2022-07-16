d0577eeeb65a551c0bd8a7a21970151178165287
cesar mayta <cesarmayta@gmail.com>
2022-07-15 21:54:02 -0500

    dia5 sistema con tablas creadas

--- semana05/dia4/pos_frontend/src/environments/environments.js
+++ semana05/dia4/pos_frontend/src/environments/environments.js
@@ -1,3 +1,4 @@
-export const URL_BACKEND = 'http://localhost:8000'
+export const URL_BACKEND = 'https://cmaytag15posbackend.herokuapp.com'
+//'http://localhost:8000'
 //'https://backposapi.herokuapp.com'
 	
