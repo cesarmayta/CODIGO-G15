aca8f4ffeee5154a97342b72ca6800e5e4a9618b
cesar mayta <cesarmayta@gmail.com>
2022-07-20 20:50:33 -0500

    dia3 registro y login api listo

--- semana06/dia3/backend/api/serializers.py
+++ semana06/dia3/backend/api/serializers.py
@@ -7,12 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username','email','password')
+        fields = ('username','first_name','last_name','email','password')
         extra_kwargs = {'password':{'write_only':True}}
 
     def create(self,validated_data):
         user = User(
             email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
             username=validated_data['username']
         )
         user.set_password(validated_data['password'])
--- semana06/dia3/backend/api/urls.py
+++ semana06/dia3/backend/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('usuario', views.UserCreateView.as_view()),
+    path('login',views.LoginView.as_view())
 ]
