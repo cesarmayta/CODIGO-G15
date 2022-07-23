48f5378e07e7651e06681edacbcc754f5520b0ac
cesar mayta <cesarmayta@gmail.com>
2022-07-23 10:56:32 -0500

    dia6 mongoengine y sqlite

--- semana06/dia6/codigomongo/codigomongo/settings.py
+++ semana06/dia6/codigomongo/codigomongo/settings.py
@@ -76,12 +76,13 @@ WSGI_APPLICATION = 'codigomongo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #ENGINE': 'django.db.backends.dummy'
     }
-}"""
+}
 
 # MONGOENGINE
 import mongoengine
