9319ebb0ea27f8f739da168f524844ff86b74ddc
cesar mayta <cesarmayta@gmail.com>
2022-08-05 20:19:27 -0500

    dia 3

---
+++ semana08/dia3/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {config} = require('./config');
+
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.json({
+        'status':true,
+        'content':'servidor activo'
+    })
+})
+
+app.listen(config.port,()=>console.log('servidor en http://localhost:'+config.port))
