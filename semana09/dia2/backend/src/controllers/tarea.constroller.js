5e32410cb47d545bd00d723bce19add96d6a7439
cesar mayta <cesarmayta@gmail.com>
2022-08-11 20:39:45 -0500

    dia2 backend

--- semana09/dia2/backend/src/controllers/tarea.constroller.js
+++ semana09/dia2/backend/src/controllers/tarea.constroller.js
@@ -1,10 +1,20 @@
 const tareaController = {};
 
-const TareaModel = require('../models/tarea.model');
+const tareaModel = require('../models/tarea.model');
 
 tareaController.getAll = async (req,res) =>{
-    const tareas = await TareaModel.find();
+    const tareas = await tareaModel.find();
     res.json(tareas);
+}
+
+tareaController.create = async (req,res) =>{
+    const {nombre,estado} = req.body;
+    const nuevaTarea = new tareaModel({
+        nombre,
+        estado
+    })
+    await nuevaTarea.save();
+    res.json(nuevaTarea);
 }
 
 module.exports = tareaController;
