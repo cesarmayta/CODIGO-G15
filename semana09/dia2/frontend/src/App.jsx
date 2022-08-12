d523614e282032cbd3c555104dfba01ecd427a6c
cesar mayta <cesarmayta@gmail.com>
2022-08-11 22:39:03 -0500

    frontend

---
+++ semana09/dia2/frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState,useEffect } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Row,Col,Card,Button,Table,Form} from 'react-bootstrap';
+import axios from 'axios';
+
+function App(){
+  const [tareas,setTareas] = useState([]);
+  const [nombre,setNombre] = useState('');
+  const [estado,setEstado] = useState('pendiente');
+
+  useEffect(()=>{
+      axios.get('http://localhost:5000/tarea')
+      .then(res=>{
+        setTareas(res.data.content);
+      })
+  },[])
+
+  return (
+    <section className="vh-100">
+      <Container className="container py-5 h-100">
+        <Row className="row d-flex justify-content-center align-items-center h-100">
+          <Col className="col col-lg-9 col-xl-7">
+            <Card className="card rounded-3">
+              <Card.Body className="card-body p-4">
+                <h4 className="text-center my-3 pb-3">Lista de Tareas</h4>
+                <Table className="table mb-4">
+                  <thead>
+                    <tr>
+                      <th scope="col">Nro.</th>
+                      <th scope="col">Tarea</th>
+                      <th scope="col">Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody id="cuerpoTabla">
+                    {tareas.map((tarea,index)=>{
+                      return (
+                        <tr key={tarea._id}>
+                            <td>{index + 1}</td>
+                            <td>{tarea.nombre}</td>
+                            <td></td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default App
