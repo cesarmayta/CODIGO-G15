418501c38ddbb5c74d74ea801e2a475395db5d08
cesar mayta <cesarmayta@gmail.com>
2022-08-15 20:10:53 -0500

    dia 4

---
+++ semana09/dia4/frontend/src/services/ProductsServices.js
@@ -0,0 +1,29 @@
+import { API_URL } from '@lib/Enviroments'
+
+export const GetAllProducts = async (preferencia_id) => {
+  let query_params = new URLSearchParams()
+  if (preferencia_id) {
+    query_params.append('preferencia', preferencia_id)
+  }
+  const response = await fetch(`${API_URL}/productos/productos/list?${query_params}`)
+  const data = await response.json()
+  return data
+}
+
+export const GetProductById = async (id) => {
+  const response = await fetch(`${API_URL}/productos/productos/${id}`)
+  const data = await response.json()
+  return data
+}
+
+export const PostProduct = async (product) => {
+  const response = await fetch(`${API_URL}/productos/productos/create`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(product)
+  })
+  const data = await response.json()
+  return data
+}
