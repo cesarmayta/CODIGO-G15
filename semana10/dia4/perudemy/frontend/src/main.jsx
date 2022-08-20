aa87e8621e0c43e2d689b474c358842f034e6025
cesar mayta <cesarmayta@gmail.com>
2022-08-20 10:29:36 -0500

    dia4

---
+++ semana10/dia4/perudemy/frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './assets/css/style.css'
+import './assets/css/icons.css'
+import './assets/css/tailwind.css'
+import './assets/css/uikit.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
