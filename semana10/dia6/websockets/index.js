4116e30e425df7e5c95b0f71416bc985683ae286
cesar mayta <cesarmayta@gmail.com>
2022-08-23 19:59:07 -0500

    dia 6

---
+++ semana10/dia6/websockets/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+const path = require('path')
+app.use(express.static(path.join(__dirname,'public')))
+
+const server = app.listen(port,()=>console.log('http://localhost:'+port))
+
+/*********** web sockets ***********/
+const SocketIO = require('socket.io');
+
+const io = SocketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('nueva conexión por web socket con id',socket.id);
+    socket.on('mensajeCliente',(data)=>{
+        console.log('mensaje del cliente : ',data)
+    })
+})
